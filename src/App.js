82ac86bb1e0670ef848fa4398d17acf4eec8cf8b
zakariabijoy <zakaria@vonome.com>
2020-09-09 04:48:03 +0600

    designing header component and also used material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Clone 🚀  </h1>
+      <Header />
     </div>
   );
 }
