06cc156c5b890d2fa5f019da0ea3e99d9eb89798
zakariabijoy <zakaria@vonome.com>
2020-09-09 03:25:52 +0600

    firebase config is added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1-9SyUWQakVkuhJYFh6wFu1vFRdAK0-w",
+    authDomain: "clone-react-app-c1497.firebaseapp.com",
+    databaseURL: "https://clone-react-app-c1497.firebaseio.com",
+    projectId: "clone-react-app-c1497",
+    storageBucket: "clone-react-app-c1497.appspot.com",
+    messagingSenderId: "535200825868",
+    appId: "1:535200825868:web:1d4cc267bb44f89312e429",
+    measurementId: "G-VBYQDPSSFW"
+};
